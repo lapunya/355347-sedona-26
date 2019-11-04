381a38c2ae0aa9d4d485360ac451039dc250925f
lapunya <53902812+lapunya@users.noreply.github.com>
2019-11-04 14:02:18 +0300

    11 commit

--- js/script.js
+++ js/script.js
@@ -1,8 +1,20 @@
 var interestBtn = document.querySelector('.interest-btn');
 var searchForm = document.querySelector('.search-form');
+var inDate = searchForm.querySelector('.in-checkin');
+var outDate = searchForm.querySelector('.in-checkout');
+
+searchForm.classList.add('search-form-hidden');
 
 interestBtn.addEventListener('click', function(evt) {
   evt.preventDefault();
-  searchForm.classList.remove('search-form');
-  searchForm.classList.add('search-form-hidden');
+  searchForm.classList.toggle('search-form-hidden');
+});
+
+searchForm.addEventListener('submit', function (evt) {
+  if (!inDate.value || !outDate.value) {
+    evt.preventDefault();
+    searchForm.classList.remove('search-form-error');
+    searchForm.offsetWidth = searchForm.offsetWidth;
+    searchForm.classList.add('search-form-error');
+  }
 });
